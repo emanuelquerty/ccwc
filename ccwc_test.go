b35f5478934a9ffc3bf76f9543056c805fe520ea
Emanuel Inacio <emanuelinacio1@hotmail.com>
2024-04-30 20:37:46 -0700

    Removes testify package and uses helper assert instead

--- ccwc_test.go
+++ ccwc_test.go
@@ -7,52 +7,47 @@ import (
 
 	"github.com/emanuelquerty/ccwc"
 	"github.com/emanuelquerty/ccwc/testdata"
-	"github.com/stretchr/testify/assert"
 )
 
 
 func TestFindByteCount(t *testing.T) {
 	errMessage := "Byte Count: the two numbers should be the same"
-	assert := assert.New(t)
 
 	for _, test := range testdata.TableDrivenData {
 		fileBytes := readFile(t, test.Filepath)
 		got := ccwc.FindByteCount(fileBytes)
-		assert.Equal(test.Expected.Bytes, got, errMessage)
+		assertHelper(t, test.Expected.Bytes, got, errMessage)
 	}
 }
 
 func TestFindLineCount(t *testing.T) {
 	errMessage := "Line Count: the two numbers should be the same"
-	assert := assert.New(t)
 
 	for _, test := range testdata.TableDrivenData {
 		fileBytes := readFile(t, test.Filepath)
 		got := ccwc.FindLineCount(fileBytes)
-		assert.Equal(test.Expected.Lines, got, errMessage)
+		assertHelper(t, test.Expected.Lines, got, errMessage)
 	}
 }
 
 func TestFindWordCount(t *testing.T) {
 	errMessage := "Word Count: the two numbers should be the same"
-	assert := assert.New(t)
 
 	
 	for _, test := range testdata.TableDrivenData {
 		fileBytes := readFile(t, test.Filepath)
 		got := ccwc.FindWordCount(fileBytes)
-		assert.Equal(test.Expected.Words, got, errMessage)
+		assertHelper(t, test.Expected.Words, got, errMessage)
 	}
 }
 
 func TestFindCharacterCount(t *testing.T) {
 	errMessage := "Chacter Count: the two numbers should be the same"
-	assert := assert.New(t)
 	
 	for _, test := range testdata.TableDrivenData {
 		fileBytes := readFile(t, test.Filepath)
 		got := ccwc.FindCharacterCount(fileBytes)
-		assert.Equal(test.Expected.Chars, got, errMessage)
+		assertHelper(t, test.Expected.Chars, got, errMessage)
 	}
 }
 
@@ -63,4 +58,11 @@ func readFile(t testing.TB, filepath string) []byte {
 		log.Fatal(err)
 	}
 	return fileBytes
+}
+
+func assertHelper(t *testing.T, expected int, got int, errMessage string) {
+	t.Helper()
+	if expected != got {
+		t.Errorf(errMessage, expected, got)
+	}
 }
